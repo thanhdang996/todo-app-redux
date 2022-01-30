3f15fe7f8dca35b99ffb7a0aa185f1bf0778d93d
thanhdang996 <82664727+thanhdang996@users.noreply.github.com>
2022-01-30 11:14:09 +0700

    changeTodoSlice

--- src/components/TodoList/TodoSlice.js
+++ src/components/TodoList/TodoSlice.js
@@ -1,7 +1,7 @@
 const initState = [
   { id: 1, name: "Learn English", completed: false, priority: "Medium" },
   { id: 2, name: "Learn React", completed: true, priority: "Low" },
-  { id: 3, name: "Learn Vue", completed: false, priority: "Hight" },
+  { id: 3, name: "Learn Vue", completed: false, priority: "High" },
 ];
 
 const todoListReducer = (state = initState, action) => {
